05c1df3386d58ed141b86dbd0fd20b51d1cb56b2
111vandit111 <vanditkala@gmail.com>
2025-06-14 01:44:12 +0530

    chores: shifiting supabase keys to env

--- src/api/Suerbase.js
+++ src/api/Suerbase.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
+const supabaseKey = import.meta.env.VITE_SUPABASE;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON;
 
-const supabaseUrl = 'https://zlzdrmiuajvziblingmb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsemRybWl1YWp2emlibGluZ21iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4MDI1NTcsImV4cCI6MjA2NTM3ODU1N30.9gHU5gdE7kYWVD-YweHLXqdnaI5eV8czbJNugsqkps8';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient( supabaseKey, supabaseAnonKey);
